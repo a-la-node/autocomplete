8230b8f1b971e381e3bf00d631246bba7db73e0a
Cleop <cleo.pearson@hotmail.co.uk>
2016-11-09 17:14:13 +0000

    js and css files display on DOM from server

--- src/server.js
+++ src/server.js
@@ -4,14 +4,24 @@ var PORT = process.env.PORT || 4000;
 
 var handler = function(request, response){
   var url = request.url;
+  var fileType = url.split('.')[1];
+
   if (url === '/'){
     fs.readFile(__dirname + '/../public/index.html', function(error, data){
       if (error){
-        console.log("Error");
+        console.log(error);
       }
       response.writeHead(200, {'content-type': 'text/html'});
       response.end(data);
     });
+  } else {
+      fs.readFile(__dirname + '/../public' + url, function(error, data){
+        if (error){
+          console.log(error);
+        }
+        response.writeHead(200, {'content-type': 'text/'+ fileType});
+        response.end(data);
+      });
   }
 }
 
