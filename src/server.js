50397b7e8544606ef5480a874245aa2ffbdac805
Cleop <cleo.pearson@hotmail.co.uk>
2016-11-09 16:22:20 +0000

    Set up server for landing page

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
+var fs = require('fs');
 var http = require('http');
 var PORT = process.env.PORT || 4000;
 
 var handler = function(request, response){
-  var endpoint = request.url;
-
+  var url = request.url;
+  if (url==='/'){
+    fs.readFile(__dirname + '../public/index.html', function(error, data){
+      if (error){
+        console.log("Error");
+      }
+      response.writeHead(200, {'content-type': 'text/html'});
+      response.end(data);
+    })
+  }
 }
 
 var server = http.createServer(handler);
-server.listen(port);
-console.log("Server is running on port: ", port);
+server.listen(PORT);
+console.log('Server is running on port: ', PORT);
