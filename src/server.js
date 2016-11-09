b5081f69bd185093165aab1e4cfb56e211fe3cba
Cleop <cleo.pearson@hotmail.co.uk>
2016-11-09 17:41:44 +0000

    Refactor, modularise handler.js and server.js

--- src/server.js
+++ src/server.js
@@ -1,30 +1,7 @@
-var fs = require('fs');
 var http = require('http');
+var runHandler = require('./handler');
 var PORT = process.env.PORT || 4000;
 
-var handler = function(request, response){
-  var url = request.url;
-  var fileType = url.split('.')[1];
-
-  if (url === '/'){
-    fs.readFile(__dirname + '/../public/index.html', function(error, data){
-      if (error){
-        console.log(error);
-      }
-      response.writeHead(200, {'content-type': 'text/html'});
-      response.end(data);
-    });
-  } else {
-      fs.readFile(__dirname + '/../public' + url, function(error, data){
-        if (error){
-          console.log(error);
-        }
-        response.writeHead(200, {'content-type': 'text/'+ fileType});
-        response.end(data);
-      });
-  }
-}
-
-var server = http.createServer(handler);
+var server = http.createServer(runHandler.handler);
 server.listen(PORT);
 console.log('Server is running on port: ', PORT);
