2909f3a64798d2dd44c7ceb007066c8d940aae80
Cleop <cleo.pearson@hotmail.co.uk>
2016-11-09 16:37:44 +0000

    Set up html page properly and visible on server

--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@ var PORT = process.env.PORT || 4000;
 
 var handler = function(request, response){
   var url = request.url;
-  if (url==='/'){
-    fs.readFile(__dirname + '../public/index.html', function(error, data){
+  if (url === '/'){
+    fs.readFile(__dirname + '/../public/index.html', function(error, data){
       if (error){
         console.log("Error");
       }
       response.writeHead(200, {'content-type': 'text/html'});
       response.end(data);
-    })
+    });
   }
 }
 
