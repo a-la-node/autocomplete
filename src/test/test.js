17bff54984ab99697d2d45fbc7f3492e3928ed01
Peter Rhodes <rhodepeter@hotmail.co.uk>
2016-11-09 16:25:45 +0000

    Add first test.

---
+++ src/test/test.js
@@ -0,0 +1,7 @@
+var test = require('tape');
+var searchFunction = require('../word-list.js');
+
+test('First test here: ', function(t){
+  t.pass('just testing that everything works');
+  t.end();
+});
