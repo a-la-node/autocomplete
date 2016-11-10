f3df62d6149d02761b7cb39768588a7477f335a4
Tom Barrett <tbtommyb@gmail.com>
2016-11-10 11:07:44 +0000

    Replaced common word list with sorted common word list and updated test accordingly

---
+++ src/test/test.js
@@ -0,0 +1,26 @@
+var test = require('tape');
+var testFuncs = require('../word-list.js');
+
+test('Matching function: ', function(t) {
+  t.deepEqual(testFuncs.smartMatch('Zyzzogeton'), ['Zyzzogeton'], 'finds one testFuncs.smartMatch for Zyzzogeton');
+
+  t.equal(testFuncs.smartMatch('abba').length, 10, 'finds ten testFuncs.smartMatches for abba');
+
+  var expected = ['lionheart', 'lionhearted', 'lionheartedness'];
+  t.deepEqual(testFuncs.smartMatch('lionheart'), expected, 'finds three testFuncs.smartMatches for lionheartedness');
+
+  t.deepEqual(testFuncs.smartMatch('dfkghdfgkjhdfghkjgadfjkh'), [], 'returns an empty array for nonsense input');
+  
+  t.end();
+});
+
+test('Matching function: ', function(t) {
+  t.equal(testFuncs.smartMatch('fi')[0], 'field', 'returns "first" rather than "fi"');
+  t.end();
+});
+
+test('Validate function: ', function(t) {
+  t.equal(testFuncs.validateInput('i£$%$^&*[c hk1345'), 'ichk', 'returns input with only letters');
+  t.equal(testFuncs.validateInput('FVNT435£%! '), 'FVNT', 'correctly validates when given capitals');
+  t.end();
+});
--- src/word-list.js
+++ src/word-list.js
@@ -1,12 +1,29 @@
 var fs = require('fs');
 
-var wordlist = fs.readFileSync('dictionary.txt', 'utf8').split('\n');
+var wordlist = fs.readFileSync(__dirname + '/dictionary.txt', 'utf8').split('\n');
+var mostCommonWordlist = fs.readFileSync(__dirname + '/1000_sorted.txt', 'utf8').split('\n');
 
-function match(input) {
-  var regex = new RegExp('^' + input, 'i');
-  return wordlist.filter(function(word) { return regex.test(word); });
+function validateInput(query) {
+  return query.replace(/[^a-z]/gi, '');
+}
+
+function makeRegExp(input) {
+  return new RegExp('^' + input, 'i');
+}
+
+function match(list, regex) {
+  return list.filter(function(word) { return regex.test(word); });
+}
+
+function smartMatch(input){
+  var regex = makeRegExp(validateInput(input));
+  var normalList = match(wordlist, regex);
+  var commonList = match(mostCommonWordlist, regex);
+  return commonList.concat(normalList);
 }
 
 module.exports = {
-  match
+  match: match,
+  validateInput: validateInput,
+  smartMatch: smartMatch
 };
