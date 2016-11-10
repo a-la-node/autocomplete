29a41bf8702377a028ca7d04dfa39a50cfd67335
Peter Rhodes <rhodepeter@hotmail.co.uk>
2016-11-10 10:23:20 +0000

    Add smarter search

--- src/word-list.js
+++ src/word-list.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 var wordlist = fs.readFileSync(__dirname + '/dictionary.txt', 'utf8').split('\n');
+var mostCommonWordlist = fs.readFileSync(__dirname + '/1000_most_common_words.txt', 'utf8').split('\n');
 
 function validateInput(query) {
   return query.replace(/[^a-z]/gi, '');
@@ -12,7 +13,17 @@ function match(input) {
   return wordlist.filter(function(word) { return regex.test(word); });
 }
 
+function smartMatch(input){
+	var regex = new RegExp('^' + input, 'i');
+	var normalList = wordlist.filter(function(word) { return regex.test(word); });
+	var commonList = mostCommonWordlist.filter(function(word) { return regex.test(word); })
+	var result = commonList;
+	normalList.forEach(function(a){result.push(a)})
+	console.log(result)
+};
+
 module.exports = {
   match: match,
-  validateInput: validateInput
+  validateInput: validateInput,
+  smartMatch: smartMatch
 };
