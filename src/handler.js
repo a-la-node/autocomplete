f060d99aa361973f744a39017934f012c513e932
Tom Barrett <tbtommyb@gmail.com>
2016-11-14 15:50:11 +0000

    Changed server tests to not rely on 'handler' function returning strings

--- src/handler.js
+++ src/handler.js
@@ -5,21 +5,21 @@ var handler = function(request, response){
   var url = request.url;
   if (url === '/'){
     handlerForLandingPage(request, response);
-    return 'handlerForLandingPage has been called';
   } else if(/search/.test(url)) {
     handlerForSearch(request, response);
   } else if(/test/.test(url)) {
     handleTest(request, response);
   } else {
     handlerForAllPages(request, response);
-    return 'handlerForAllPages has been called';
   }
 };
 
 function handlerForLandingPage (request, response) {
   fs.readFile(__dirname + '/../public/index.html', function(error, data){
     if (error){
-      throw error;
+      response.writeHead(404, {'content-type': 'text/html'});
+      response.end('404 Not Found\n');
+      return;
     }
     response.writeHead(200, {'content-type': 'text/html'});
     response.end(data);
@@ -44,12 +44,12 @@ function handlerForSearch (request, response) {
 
 function handlerForAllPages(request, response) {
   var url = request.url;
-  console.log(url);
   var fileType = url.split('.')[1];
   fs.readFile(__dirname + '/../public' + url, function(error, data){
-    console.log(__dirname + '/../public' + url, 'dirname');
     if (error){
-      throw error;
+      response.writeHead(404, {'content-type': 'text/html'});
+      response.end('404 Not Found\n');
+      return;
     }
     response.writeHead(200, {'content-type': 'text/'+ fileType});
     response.end(data);
--- src/test/server.test.js
+++ src/test/server.test.js
@@ -1,17 +1,71 @@
 var test = require('tape');
 var handler = require('../handler').handler;
+var fs = require('fs');
 
-var mockResponse = {
-  writeHead : function(){},
-  end : function(){}
-};
-
-test('Test if inputting / calls handlerForLandingPage function', function(t) {
-  t.equal(handler({url:'/'}, mockResponse), 'handlerForLandingPage has been called');
+test('Inputting / returns the index.html', function(t) {
+  var indexFile = fs.readFileSync(__dirname + '/../../public/index.html');
+  handler({url: '/'}, {
+    writeHead: function(status, obj) {
+      t.equal(status, 200);
+      t.deepEqual(obj, {'content-type': 'text/html'});
+    },
+    end: function(data) {
+      t.deepEqual(data, indexFile);
+    }
+  });
   t.end();
 });
 
-test('Test that inputting /style.css calls handlerForAllPages function', function(t) {
-  t.equal(handler({url:'/styles/style.css'}, mockResponse), 'handlerForAllPages has been called');
+test('Inputting /styles/style.css returns the style.css', function(t) {
+  var cssFile = fs.readFileSync(__dirname + '/../../public/styles/style.css');
+  handler({url: '/styles/style.css'}, {
+    writeHead: function(status, obj) {
+      t.equal(status, 200);
+      t.deepEqual(obj, {'content-type': 'text/css'});
+    },
+    end: function(data) {
+      t.deepEqual(data, cssFile);
+    }
+  });
+  t.end();
+});
+
+test('Inputting /scripts/index.js returns the js file', function(t) {
+  var jsFile = fs.readFileSync(__dirname + '/../../public/scripts/index.js');
+  handler({url: '/scripts/index.js'}, {
+    writeHead: function(status, obj) {
+      t.equal(status, 200);
+      t.deepEqual(obj, {'content-type': 'text/js'});
+    },
+    end: function(data) {
+      t.deepEqual(data, jsFile);
+    }
+  });
+  t.end();
+});
+
+test('Inputting nonsense should return a 404', function(t) {
+  handler({url: '/kjghf/jhg'}, {
+    writeHead: function(status, obj) {
+      t.equal(status, 404);
+      t.deepEqual(obj, {'content-type': 'text/html'});
+    },
+    end: function(data) {
+      t.equal(data, '404 Not Found\n');
+    }
+  });
+  t.end();
+});
+
+test('Inputting punctuation should return a 404', function(t) {
+  handler({url: '/....'}, {
+    writeHead: function(status, obj) {
+      t.equal(status, 404);
+      t.deepEqual(obj, {'content-type': 'text/html'});
+    },
+    end: function(data) {
+      t.equal(data, '404 Not Found\n');
+    }
+  });
   t.end();
 });
